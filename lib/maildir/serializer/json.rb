d74d8625c93a1aae76c9ee6a4c6f6db1c3eeea66
Aaron Suggs <aaron@ktheory.com>
2010-12-19 09:22:46 -0500

    Prefer yajl/json_gem, but fall back to json gem

--- lib/maildir/serializer/json.rb
+++ lib/maildir/serializer/json.rb
@@ -1,13 +1,21 @@
-require 'yajl/json_gem'
-# Serialize messages as JSON
-class Maildir::Serializer::JSON < Maildir::Serializer::Base
-  # Read data from path and parse it as JSON.
-  def load(path)
-    ::JSON.load(super(path))
-  end
+# Prefer yajl JSON library
+begin
+  require 'yajl/json_gem'
+rescue LoadError
+  require 'json'
+end
 
-  # Dump data as JSON and writes it to path.
-  def dump(data, path)
-    super(data.to_json, path)
+# Serialize messages as JSON
+module Maildir::Serializer
+  class JSON < Base
+    # Read data from path and parse it as JSON.
+    def load(path)
+      ::JSON.load(super(path))
+    end
+
+    # Dump data as JSON and writes it to path.
+    def dump(data, path)
+      super(data.to_json, path)
+    end
   end
 end
