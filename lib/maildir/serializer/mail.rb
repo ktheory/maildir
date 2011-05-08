e01e0248bd45c9900688b7cb5d7396d931e36cd8
Alexander Flatter <flatter@gmail.com>
2011-05-08 19:15:44 +0200

    Update serializers to open constants

--- lib/maildir/serializer/mail.rb
+++ lib/maildir/serializer/mail.rb
@@ -1,13 +1,17 @@
 require 'mail'
-# Serialize messages as a ruby Mail object
-class Maildir::Serializer::Mail < Maildir::Serializer::Base
-  # Build a new Mail object from the data at path.
-  def load(path)
-    ::Mail.new(super(path))
-  end
+class Maildir
+  module Serializer
+    # Serialize messages as a ruby Mail object
+    class Mail < Maildir::Serializer::Base
+      # Build a new Mail object from the data at path.
+      def load(path)
+        ::Mail.new(super(path))
+      end
 
-  # Write data to path as a Mail message.
-  def dump(data, path)
-    super(data.to_s, path)
+      # Write data to path as a Mail message.
+      def dump(data, path)
+        super(data.to_s, path)
+      end
+    end
   end
 end
--- lib/maildir/serializer/marshal.rb
+++ lib/maildir/serializer/marshal.rb
@@ -1,12 +1,16 @@
-# Serialize messages as Marshalled ruby objects
-class Maildir::Serializer::Marshal < Maildir::Serializer::Base
-  # Read data from path and unmarshal it.
-  def load(path)
-    ::Marshal.load(super(path))
-  end
+class Maildir
+  module Serializer
+    # Serialize messages as Marshalled ruby objects
+    class Marshal < Maildir::Serializer::Base
+      # Read data from path and unmarshal it.
+      def load(path)
+        ::Marshal.load(super(path))
+      end
 
-  # Marshal data and write it to path.
-  def dump(data, path)
-    super(::Marshal.dump(data), path)
+      # Marshal data and write it to path.
+      def dump(data, path)
+        super(::Marshal.dump(data), path)
+      end
+    end
   end
 end
