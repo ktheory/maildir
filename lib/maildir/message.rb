6931456532abe70c242dd8130021ffd4ed76d48a
Todd A. Jacobs <codegnome@address-is.invalid>
2020-06-24 17:14:09 -0400

    replace call to Object#blank?
    - Predicate if-statement checks for nil and empty string/array directly,
      rather than relying on ActiveSupport's Object#blank? method.
    - Could be simplified further using safe navigation in Ruby >= 2.3, but
      this ensures backwards compatibility with Ruby >= 2.0.0.

--- lib/maildir/message.rb
+++ lib/maildir/message.rb
@@ -142,7 +142,7 @@ class Maildir::Message
   #
   # flags:: String or Array
   def remove_flag(flags)
-    return self.flags if flags.blank?
+    return self.flags if !flags || flags.empty?
     self.flags = self.flags.reject { |f| f =~ /[#{Array(flags).join}]/i }
   end
 
